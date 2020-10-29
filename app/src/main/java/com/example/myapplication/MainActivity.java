a058d6d00a901b5f90a56279d41793902aba47c0
tutatj84 <tiennguyenanh84@gmail.com>
2020-10-29 10:53:43 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
+	}
+}
